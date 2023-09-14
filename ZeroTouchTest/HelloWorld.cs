04a7494ae319861961a9168d81d6a10db5f5ac06
Krachkovskii <117347760+Krachkovskii@users.noreply.github.com>
2023-09-14 13:50:48 +0400

    Transferred from internal storage

---
+++ ZeroTouchTest/HelloWorld.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroTouchNodes
+{
+    public class NumberFuckery
+    {
+        private NumberFuckery()
+        {
+
+        }
+
+        public double MultiplyTwoNumbers(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double DivideTwoNumbers(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+    }
+
+    public class StringFuckery
+    {
+        private string _InputString
+        {
+            get { return _InputString; }
+        }
+        private StringFuckery()
+        {
+
+        }
+
+        public string AddFuck(string start)
+        {
+            return start + " Fuck";
+        }
+
+        public string RemoveFuck(string start)
+        {
+            return start.Replace("Fuck", "");
+        }
+    }
+}
---
+++ ZeroTouchTest/RevitWall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB = Autodesk.Revit.DB;
+
+namespace ZeroTouchTest
+{
+    internal class RevitWall
+    {
+        private RevitWall(){}
+
+        public DB.Wall Create()
+        {
+            DB.Wall NewWall = DB.Wall.Create(DB.Document doc, DB.Curve curve, DB.ElementId typeId, DB.ElementId levelId, double height, double offset, bool flip, bool structural);
+            return NewWall;
+        }
+    }
+}
